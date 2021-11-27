0270c82977826f8bb33ece2f76ac566c2b4e6ec4
mcarrington1 <mcarrington1@live.maryville.edu>
2021-11-27 13:35:35 -0600

    undid weirdness with POM and object returns in a test

--- src/main/java/tests/TestE2ESubmitPaymentWithCamera.java
+++ src/main/java/tests/TestE2ESubmitPaymentWithCamera.java
@@ -13,19 +13,20 @@ import validations.PayPageValidation;
 
 public class TestE2ESubmitPaymentWithCamera extends TestBase {
     private final String expectedBillAmount = "5.00";
-    private final String expectedBillerName = "City Of Tallahassee";
     private final BankAccount bankAccount = new BankAccount();
     private final Address address = new Address();
 
     @Test(description = "Execute an E2E test with a utility bill, including payment setup and submission.")
     public void testSendingUtilityBill() {
+        String expectedBillerName = "City Of Tallahassee";
         AvdManagement.setTestImage(BillType.UTILITY);
 
+        // Initiate Payment Process
         SplashPage splashPage = new SplashPage(driver);
+        splashPage.startPayBill();
 
-        CaptureBillPage captureBillPage = splashPage.startPayBill();
-
-        // Capture Bill Image & set amount
+        // Capture our payment and submit bill amount
+        CaptureBillPage captureBillPage = new CaptureBillPage(driver);
         captureBillPage
                 .authorizeCameraAccess()
                 .captureBillFromCamera()
@@ -33,29 +34,35 @@ public class TestE2ESubmitPaymentWithCamera extends TestBase {
 
         CaptureBillPageValidation.validateBillAmount(captureBillPage, "$" + expectedBillAmount);
         CaptureBillPageValidation.validateBillerName(captureBillPage, expectedBillerName);
+        captureBillPage.addBankAccount();
 
-        AddBankAccountPage addBankAccountPage = captureBillPage.addBankAccount();
-
-        // Add Bank Account with Address
-        BillingAddressPage billingAddressPage = addBankAccountPage
+        // Submit our bank account information
+        AddBankAccountPage addBankAccountPage = new AddBankAccountPage(driver);
+        addBankAccountPage
                 .enterBankAccountDetails(bankAccount)
                 .submitBankAccount();
 
-        PayPage payPage = billingAddressPage
+        // Submit and confirm our billing information
+        BillingAddressPage billingAddressPage = new BillingAddressPage(driver);
+        billingAddressPage
                 .setAddress(address)
                 .acceptTos()
                 .saveBankAccountAndSubmit();
 
+        // Validate our payment and submit
+        PayPage payPage = new PayPage(driver);
         PayPageValidation.validatePartialAccountNumber(payPage, bankAccount.getAccountNumber());
-
-        // Submit Payment
         //TODO: These are commented out so we do not spam the server
-//        ConfirmationPage confirmationPage = payPage.submitPayment();
+//        payPage.submitPayment();
+//        // Confirm successful payment
+//        ConfirmationPage confirmationPage = new ConfirmationPage(driver);
 //        ConfirmationPageValidation.validateConfirmationPageSuccess(confirmationPage);
     }
 
     @Test
     public void testSendingMedicalBill() {
+        String expectedBillerName = "Your Bill";
+
         AvdManagement.setTestImage(BillType.MEDICAL);
 
 
